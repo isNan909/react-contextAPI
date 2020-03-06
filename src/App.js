fda4e84fdf776095cec6bfc6428f7a1cceb32bab
Ishan Manandhar <ishan02016@gmail.com>
2020-03-06 20:39:05 +0545

    fixing .gitignore

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './stylesheet/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto">
+        <h3 className="text-center  text-2xl mt-20 text-base leading-8 text-indigo-600 font-semibold tracking-wide uppercase">CRUD Application with Context API</h3>
+        {/* other subcomponents stays here  */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
